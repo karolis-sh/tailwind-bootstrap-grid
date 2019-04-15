c1941dad79aa3216305550f7fefba379d8bdacc9
Karolis Šarapnickis <pastas.k@gmail.com>
2019-04-15 22:04:40 +0300

    test: add tests for css generation

---
+++ tests/index.test.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const postcss = require('postcss');
+const tailwindcss = require('tailwindcss');
+
+const CSS_INPUT = `
+@tailwind components;
+@tailwind utilities;
+`;
+
+describe('tailwind-bootstrap-grid', () => {
+  it('should handle defaults', async () => {
+    const result = await postcss()
+      .use(tailwindcss(path.resolve(__dirname, 'tailwind.js')))
+      .process(CSS_INPUT, { from: undefined });
+
+    expect(result.warnings().length).toBe(0);
+    expect(result.css).toMatchSnapshot();
+  });
+
+  it('should handle custom prefix and separator', async () => {
+    const result = await postcss()
+      .use(tailwindcss(path.resolve(__dirname, 'tailwind.prefix-separator.js')))
+      .process(CSS_INPUT, { from: undefined });
+
+    expect(result.warnings().length).toBe(0);
+    expect(result.css).toMatchSnapshot();
+  });
+});
