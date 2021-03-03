c3d7504277bfd617edd783cf4fa410e3c826d139
Karolis Šarapnickis <pastas.k@gmail.com>
2021-03-03 23:00:48 +0200

    test: add !important test

---
+++ tests/tailwind.important.js
@@ -0,0 +1,14 @@
+/* eslint-disable global-require */
+const config = require('./tailwind.base');
+
+module.exports = {
+  ...config,
+  theme: {
+    screens: {
+      mobile: '1em',
+      tablet: '2em',
+      desktop: '3em',
+    },
+  },
+  plugins: [require('../lib')({ containerMaxWidths: {} })],
+};
