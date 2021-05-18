161160cfec3802302db8386d34535ee421ece656
Karolis Šarapnickis <pastas.k@gmail.com>
2021-05-19 00:03:02 +0300

    chore: upgrade demo setup and editor style

--- demo/postcss.config.js
+++ demo/postcss.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const tailwindcss = require('tailwindcss');
-const cssnano = require('cssnano');
-const formatCss = require('stylefmt');
 
 module.exports = {
-  plugins: [tailwindcss(path.resolve(__dirname, 'tailwind.js')), cssnano(), formatCss],
+  plugins: [tailwindcss(path.resolve(__dirname, 'tailwind.js'))],
 };
---
+++ demo/src/theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  plain: {
+    backgroundColor: '#f7f7f9',
+    color: '#212529',
+  },
+  styles: [
+    {
+      types: ['tag'],
+      style: {
+        color: '#2f6f9f',
+      },
+    },
+    {
+      types: ['attr-name', 'attr-equals'],
+      style: {
+        color: '#4f9fcf',
+      },
+    },
+    {
+      types: ['attr-value'],
+      style: {
+        color: '#d44950',
+      },
+    },
+  ],
+};
+
+export default theme;
