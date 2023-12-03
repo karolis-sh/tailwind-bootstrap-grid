2bef8b9e620332de9f7c55ad9cf13002503926b0
Karolis Šarapnickis <hi@karolis.sh>
2023-12-03 23:54:00 +0200

    test: use vitest instead of jest

--- tests/tailwind.prefix-separator.test.js
+++ tests/tailwind.prefix-separator.test.js
@@ -1,4 +1,4 @@
 const run = require('./run-test');
 
-// eslint-disable-next-line jest/expect-expect
+// eslint-disable-next-line vitest/expect-expect
 it('should handle custom prefix and separator', () => run(__filename));
